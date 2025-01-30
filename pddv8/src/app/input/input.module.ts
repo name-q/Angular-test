5ce8649d973dda73d08c373bdd64736770dee191
name-q <1620206666@qq.com>
2025-01-30 16:02:31 +0800

    no message

--- pddv8/src/app/input/input.module.ts
+++ pddv8/src/app/input/input.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { InputComponent } from './input.component';
 // import { InfoRoutingModule } from './input-routing.module';
 import { InputRoutes } from './input.routing';
