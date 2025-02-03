e32769256869c77a65e2a07542dce770173f5bfc
name-q <1620206666@qq.com>
2025-02-03 22:00:59 +0800

    rxjs

--- h5pdd/src/app/home/components/home-detail/home-detail.component.ts
+++ h5pdd/src/app/home/components/home-detail/home-detail.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { Channel, ImageSlider } from 'src/app/components';
 import { HomeService } from 'src/app/shared/services';
 
@@ -9,42 +11,60 @@ import { HomeService } from 'src/app/shared/services';
   styleUrls: ['./home-detail.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeDetailComponent implements OnInit {
+export class HomeDetailComponent implements OnInit, OnDestroy {
 
-  sliders: ImageSlider[] = [];
+  sliders$: Observable<ImageSlider[]>;
 
   channels: Channel[] = [];
 
-  selectTabLink = '';
+  selectTabLink$: Observable<string>;
   constructor(
-    private router: ActivatedRoute, 
+    private router: ActivatedRoute,
     private cd: ChangeDetectorRef,
     private service: HomeService
   ) { }
 
+  sub:Subscription;
   ngOnInit() {
     // 一次获取
     // this.selectTabLink = this.router.snapshot.params.tabLink;
     // 监听变更获取
-    this.router.paramMap.subscribe(params => {
-      this.selectTabLink = params.get('tabLink');
-      // 手动提醒变化 （非@input变化 onPush模式下，不会自动更新）
-      this.cd.markForCheck()
-    })
-    this.service.getBanners().subscribe(res => {
-      if(res.data) {
-        this.sliders = res.data;
-        this.cd.markForCheck()
-      }
-    })
+    // this.router.paramMap.subscribe(params => {
+    //   this.selectTabLink = params.get('tabLink');
+    //   // 手动提醒变化 （非@input变化 onPush模式下，不会自动更新）
+    //   this.cd.markForCheck()
+    // })
+    this.selectTabLink$ = this.router.paramMap
+      .pipe(
+        filter(params => params.has('tabLink')),
+        map(params => {
+          return params.get('tabLink');
+        })
+      )
+
+    this.sliders$ = this.service.getBanners().pipe(
+      filter(res => res.data !== null),
+      map(res => res.data) // 转换为 ImageSlider 数组
+    );
 
     this.service.getChannels().subscribe(res => {
-      if(res.data) {
+      if (res.data) {
         this.channels = res.data;
         this.cd.markForCheck()
       }
     })
+
+
+    // 此处不使用rxjs要关闭监听防止内存泄漏
+    this.sub = this.router.paramMap.subscribe(params => console.log(params))
+
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+
 
 
 }
