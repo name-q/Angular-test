43fb41770be95239ff2d81b31438040df8965ab9
name-q <1620206666@qq.com>
2025-02-03 19:18:29 +0800

    hot update mockapi server

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mockData = require('../utils/mockData');
 
-router.post('/mock-post', (req, res) => {
+router.post('/api/getTabList', (req, res) => {
   res.json({
     message: 'This is a mock POST response',
     data: mockData.getTabList()
